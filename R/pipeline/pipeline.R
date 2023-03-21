4139051bd7982fb805eb4f7c6d73a0a55d330db1
Selina Baldauf <selina.baldauf@gmx.de>
2023-03-21 13:23:49 +0100

    Embed resources

--- R/pipeline/pipeline.R
+++ R/pipeline/pipeline.R
@@ -1,14 +1,13 @@
 # render the website
-
 quarto::quarto_render()
 
 # render slides as pdf
 slides_html <- list.files(here::here("_site/slides"),
-                          pattern = "slide", full.names = TRUE
+  pattern = "slide", full.names = TRUE
 )
 
 lapply(slides_html, function(x) {
   pagedown::chrome_print(x,
-                         format = "pdf"
+    format = "pdf"
   )
 })
